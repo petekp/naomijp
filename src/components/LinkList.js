692b4d414edef35a0d5061faa0e8e61584668303
petekp <pete.petrash@gmail.com>
2016-07-30 15:46:09 -0700

    add react router, componetize existing elements

---
+++ src/components/LinkList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router'
+
+const LinkList = (props) => {
+  return (
+    <section className="links">
+      <div className="LinkList">
+        <Link to="/">Home</Link>
+        <Link to="/writing">Writing</Link>
+        <Link to="/links">Links</Link>
+      </div>
+    </section>
+  )
+}
+
+export default LinkList;
